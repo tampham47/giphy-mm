2c06c1ec2faa404f000c5dc87cc19cf40eb54086
Tam Pham <tampham47@live.com>
2019-07-09 21:35:48 +0700

    using styled-component instead of css components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
 import React from 'react';
+import styled, { createGlobalStyle, keyframes } from 'styled-components';
 import logo from './logo.svg';
-import './App.css';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+      monospace;
+  }
+`;
+
+const logoSpin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Main = styled.div`
+  text-align: center;
+`;
+const Header = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+const Logo = styled.img`
+  animation: ${logoSpin} infinite 20s linear;
+  height: 40vmin;
+  pointer-events: none;
+`;
+const Link = styled.a`
+  color: #61dafb;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Main>
+      <GlobalStyle />
+      <Header>
+        <Logo src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
+        <Link
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
+        </Link>
+      </Header>
+    </Main>
   );
 }
 
